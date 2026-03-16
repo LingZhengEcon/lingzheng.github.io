cbc0400a0b1c5dca776e0fad3143d0822be5de5e
LingZhengEcon <147572005+LingZhengEcon@users.noreply.github.com>
2026-03-16 08:56:32 +0000

    Deploying to gh-pages from @ LingZhengEcon/LingZhengEcon.github.io@e75b48ec5a581079b9a92c1b282d2d1b68386f8a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-i-am-trilled-to-begin-my-master-s-studies-at-the-chinese-university-of-hong-kong-and-northwestern-university-s-dmdp-program-in-august",
+          title: 'I am trilled to begin my Master’s studies at the Chinese University of...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
